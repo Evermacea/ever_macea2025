83470bd3404a537a32fff6a7d66f62fcd9cb8691
Ever Macea <ever.macea@est.iudigital.edu.co>
2025-02-27 22:15:43 -0500

    Eliminar venv del repositorio

---
+++ actividad_1.py
@@ -0,0 +1,10 @@
+from clase_1 import Personas
+persona = Personas()
+persona.nombre = "Otoniel Vazquez"
+persona.edad = 25
+persona.estatura = 1.80
+persona.peso = 70
+print("Nombre: ",persona.nombre)
+print("Edad: ",persona.edad)
+print("Estarura :",persona.estatura)
+print("Peso: ",persona.peso)
